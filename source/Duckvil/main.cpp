cfd818e020a0deccc64d5719b91752f816f5e8e0
mVento3 <aleksander.lipka06@wp.pl>
2020-09-08 13:20:49 +0200

    Command line arguments parsing

--- source/Duckvil/main.cpp
+++ source/Duckvil/main.cpp
@@ -1,8 +1,13 @@
 #include <cstdio>
 
+#include "Utils/CommandArgumentsParser.h"
+#include "Utils/Macro.h"
+
+Duckvil::Utils::CommandArgumentsParser::Descriptor* g_pDescriptors = { 0 };
+
 int main(int argc, char* argv[])
 {
-    printf("Hello Duckvil!");
+    Duckvil::Utils::CommandArgumentsParser _parser(argc, argv, g_pDescriptors, DUCKVIL_ARRAY_SIZE(g_pDescriptors));
 
     return 0;
 }
