f1fe7ac13e45130520163000bc372a01c01c104a
mVento3 <aleksander.lipka06@wp.pl>
2020-09-12 10:28:34 +0200

    DLL export for linux

--- include/Utils/Macro.h
+++ include/Utils/Macro.h
@@ -2,4 +2,11 @@
 
 #define DUCKVIL_ARRAY_SIZE(arr) (sizeof(arr) / sizeof(arr[0]))
 #define DUCKVIL_CAT(a, b) a ## b
+
+#ifdef DUCKVIL_PLATFORM_WINDOWS
 #define DUCKVIL_EXPORT extern "C" __declspec(dllexport)
+#else
+#ifdef DUCKVIL_PLATFORM_LINUX
+#define DUCKVIL_EXPORT extern "C" __attribute__ ((dllexport))
+#endif
+#endif
