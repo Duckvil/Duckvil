3931487eba6fe0ea22d88e2fd45264e5eb299cc8
mVento3 <aleksander.lipka06@wp.pl>
2020-09-14 17:33:34 +0200

    Added FixedStack allocation tests

--- test/TestMemory/StackAllocation.cpp
+++ test/TestMemory/StackAllocation.cpp
@@ -6,13 +6,14 @@
 
 #include <cstring>
 
-DUCKVIL_TEST(StackAllocation)
+DUCKVIL_TEST(FixedStackAllocation)
 {
+    Duckvil::Memory::Stack<int> _stack(__duckvil_global::m_pMemoryInterface, (Duckvil::Memory::__allocator*)__duckvil_global::m_memoryChunk, 4);
+
+    DUCKVIL_TEST_EXP(_stack.Empty(), "Stack is not empty");
+
+// Contiguous allocation
     {
-        Duckvil::Memory::Stack<int> _stack(__duckvil_global::m_pMemoryInterface, (Duckvil::Memory::__allocator*)__duckvil_global::m_memoryChunk, 4);
-
-        DUCKVIL_TEST_EXP(_stack.Empty(), "Stack is not empty");
-
         _stack.Allocate(1);
         _stack.Allocate(2);
         _stack.Allocate(3);
@@ -35,17 +36,62 @@ DUCKVIL_TEST(StackAllocation)
         DUCKVIL_TEST_EXP(_stack.Empty(), "Stack is not empty");
     }
 
+// Commutative allocation
     {
-        Duckvil::Memory::Stack<int> _stack(__duckvil_global::m_pMemoryInterface, (Duckvil::Memory::__allocator*)__duckvil_global::m_memoryChunk, 100);
-
-        DUCKVIL_TEST_EXP(_stack.Empty(), "Stack is not empty");
-
-        for(uint32_t i = 0; i < 100; i++)
         {
-            _stack.Allocate(i);
+            _stack.Allocate(1);
+
+            DUCKVIL_TEST_EQUAL(_stack.Top(), 1, "Wrong value");
+            _stack.Pop();
+
+            _stack.Allocate(2);
+
+            DUCKVIL_TEST_EQUAL(_stack.Top(), 2, "Wrong value");
+            _stack.Pop();
+
+            _stack.Allocate(3);
+
+            DUCKVIL_TEST_EQUAL(_stack.Top(), 3, "Wrong value");
+            _stack.Pop();
+
+            _stack.Allocate(4);
+
+            DUCKVIL_TEST_EQUAL(_stack.Top(), 4, "Wrong value");
+            _stack.Pop();
         }
 
-        DUCKVIL_TEST_EXP(_stack.Full(), "Stack is not full");
+        {
+            _stack.Allocate(1);
+            _stack.Allocate(2);
+
+            DUCKVIL_TEST_EQUAL(_stack.Top(), 2, "Wrong value");
+            _stack.Pop();
+
+            DUCKVIL_TEST_EQUAL(_stack.Top(), 1, "Wrong value");
+            _stack.Pop();
+
+            DUCKVIL_TEST_EXP(_stack.Empty(), "Stack is not empty");
+        }
+
+        {
+            for(int i = 0; i < 100; i++)
+            {
+                _stack.Allocate(1 * i);
+                _stack.Allocate(2 * i);
+                _stack.Allocate(3 * i);
+
+                DUCKVIL_TEST_EQUAL(_stack.Top(), 3 * i, "Wrong value");
+                _stack.Pop();
+
+                DUCKVIL_TEST_EQUAL(_stack.Top(), 2 * i, "Wrong value");
+                _stack.Pop();
+
+                DUCKVIL_TEST_EQUAL(_stack.Top(), 1 * i, "Wrong value");
+                _stack.Pop();
+
+                DUCKVIL_TEST_EXP(_stack.Empty(), "Stack is not empty");
+            }
+        }
     }
 
     DUCKVIL_TEST_SUCCESS_PASS;
