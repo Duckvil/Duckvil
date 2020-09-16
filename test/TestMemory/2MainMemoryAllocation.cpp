54c7e70e8ead14f1628df3452990150c273cb007
mVento3 <aleksander.lipka06@wp.pl>
2020-09-16 09:42:13 +0200

    Slightly changed memory tests

--- test/TestMemory/2MainMemoryAllocation.cpp
+++ test/TestMemory/2MainMemoryAllocation.cpp
@@ -6,45 +6,47 @@
 
 DUCKVIL_TEST(MemoryAlignmentCalculating)
 {
-    DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding(0x0000000000000000, 1), (uint8_t)0, "Wrong padding");
-    DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding(0x0000000000000000, 2), (uint8_t)0, "Wrong padding");
-    DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding(0x0000000000000000, 4), (uint8_t)0, "Wrong padding");
-    DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding(0x0000000000000000, 8), (uint8_t)0, "Wrong padding");
+    {
+        DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding_((const void*)0x0000000000000000, 1), (uint8_t)0, "Wrong padding");
+        DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding_((const void*)0x0000000000000000, 2), (uint8_t)0, "Wrong padding");
+        DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding_((const void*)0x0000000000000000, 4), (uint8_t)0, "Wrong padding");
+        DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding_((const void*)0x0000000000000000, 8), (uint8_t)0, "Wrong padding");
 
-    DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding(0x0000000000000001, 1), (uint8_t)0, "Wrong padding");
-    DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding(0x0000000000000001, 2), (uint8_t)1, "Wrong padding");
-    DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding(0x0000000000000001, 4), (uint8_t)3, "Wrong padding");
-    DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding(0x0000000000000001, 8), (uint8_t)7, "Wrong padding");
+        DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding_((const void*)0x0000000000000001, 1), (uint8_t)0, "Wrong padding");
+        DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding_((const void*)0x0000000000000001, 2), (uint8_t)1, "Wrong padding");
+        DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding_((const void*)0x0000000000000001, 4), (uint8_t)3, "Wrong padding");
+        DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding_((const void*)0x0000000000000001, 8), (uint8_t)7, "Wrong padding");
 
-    DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding(0x0000000000000002, 1), (uint8_t)0, "Wrong padding");
-    DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding(0x0000000000000002, 2), (uint8_t)0, "Wrong padding");
-    DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding(0x0000000000000002, 4), (uint8_t)2, "Wrong padding");
-    DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding(0x0000000000000002, 8), (uint8_t)6, "Wrong padding");
+        DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding_((const void*)0x0000000000000002, 1), (uint8_t)0, "Wrong padding");
+        DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding_((const void*)0x0000000000000002, 2), (uint8_t)0, "Wrong padding");
+        DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding_((const void*)0x0000000000000002, 4), (uint8_t)2, "Wrong padding");
+        DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding_((const void*)0x0000000000000002, 8), (uint8_t)6, "Wrong padding");
 
-    DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding(0x0000000000000003, 1), (uint8_t)0, "Wrong padding");
-    DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding(0x0000000000000003, 2), (uint8_t)1, "Wrong padding");
-    DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding(0x0000000000000003, 4), (uint8_t)1, "Wrong padding");
-    DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding(0x0000000000000003, 8), (uint8_t)5, "Wrong padding");
+        DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding_((const void*)0x0000000000000003, 1), (uint8_t)0, "Wrong padding");
+        DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding_((const void*)0x0000000000000003, 2), (uint8_t)1, "Wrong padding");
+        DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding_((const void*)0x0000000000000003, 4), (uint8_t)1, "Wrong padding");
+        DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding_((const void*)0x0000000000000003, 8), (uint8_t)5, "Wrong padding");
 
-    DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding(0x0000000000000004, 1), (uint8_t)0, "Wrong padding");
-    DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding(0x0000000000000004, 2), (uint8_t)0, "Wrong padding");
-    DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding(0x0000000000000004, 4), (uint8_t)0, "Wrong padding");
-    DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding(0x0000000000000004, 8), (uint8_t)4, "Wrong padding");
+        DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding_((const void*)0x0000000000000004, 1), (uint8_t)0, "Wrong padding");
+        DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding_((const void*)0x0000000000000004, 2), (uint8_t)0, "Wrong padding");
+        DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding_((const void*)0x0000000000000004, 4), (uint8_t)0, "Wrong padding");
+        DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding_((const void*)0x0000000000000004, 8), (uint8_t)4, "Wrong padding");
 
-    DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding(0x0000000000000005, 1), (uint8_t)0, "Wrong padding");
-    DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding(0x0000000000000005, 2), (uint8_t)1, "Wrong padding");
-    DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding(0x0000000000000005, 4), (uint8_t)3, "Wrong padding");
-    DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding(0x0000000000000005, 8), (uint8_t)3, "Wrong padding");
+        DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding_((const void*)0x0000000000000005, 1), (uint8_t)0, "Wrong padding");
+        DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding_((const void*)0x0000000000000005, 2), (uint8_t)1, "Wrong padding");
+        DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding_((const void*)0x0000000000000005, 4), (uint8_t)3, "Wrong padding");
+        DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding_((const void*)0x0000000000000005, 8), (uint8_t)3, "Wrong padding");
 
-    DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding(0x0000000000000006, 1), (uint8_t)0, "Wrong padding");
-    DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding(0x0000000000000006, 2), (uint8_t)0, "Wrong padding");
-    DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding(0x0000000000000006, 4), (uint8_t)2, "Wrong padding");
-    DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding(0x0000000000000006, 8), (uint8_t)2, "Wrong padding");
+        DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding_((const void*)0x0000000000000006, 1), (uint8_t)0, "Wrong padding");
+        DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding_((const void*)0x0000000000000006, 2), (uint8_t)0, "Wrong padding");
+        DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding_((const void*)0x0000000000000006, 4), (uint8_t)2, "Wrong padding");
+        DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding_((const void*)0x0000000000000006, 8), (uint8_t)2, "Wrong padding");
 
-    DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding(0x0000000000000007, 1), (uint8_t)0, "Wrong padding");
-    DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding(0x0000000000000007, 2), (uint8_t)1, "Wrong padding");
-    DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding(0x0000000000000007, 4), (uint8_t)1, "Wrong padding");
-    DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding(0x0000000000000007, 8), (uint8_t)1, "Wrong padding");
+        DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding_((const void*)0x0000000000000007, 1), (uint8_t)0, "Wrong padding");
+        DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding_((const void*)0x0000000000000007, 2), (uint8_t)1, "Wrong padding");
+        DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding_((const void*)0x0000000000000007, 4), (uint8_t)1, "Wrong padding");
+        DUCKVIL_TEST_EQUAL(__duckvil_global::m_pMemoryInterface->m_fnCalculatePadding_((const void*)0x0000000000000007, 8), (uint8_t)1, "Wrong padding");
+    }
 
     DUCKVIL_TEST_SUCCESS_PASS;
 }
