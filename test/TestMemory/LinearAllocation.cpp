ad0c5bc19506864a90b7bc4ef8ccf4748f708471
mVento3 <aleksander.lipka06@wp.pl>
2020-09-25 08:57:16 +0200

    Linear unit tests

--- test/TestMemory/LinearAllocation.cpp
+++ test/TestMemory/LinearAllocation.cpp
@@ -17,14 +17,16 @@ DUCKVIL_TEST(LinearAllocation)
     {
         Duckvil::Memory::__linear_allocator* _linear = __duckvil_global::m_pMemoryInterface->m_fnAllocateLinearAllocator((Duckvil::Memory::__allocator*)__duckvil_global::m_memoryChunk, 16);
 
-        DUCKVIL_TEST_EXP(_linear->used == 0, "Linear allocator is not empty");
+        DUCKVIL_TEST_EXP(Duckvil::Memory::linear_empty(_linear), "Linear allocator is not empty");
+        DUCKVIL_TEST_EQUAL(Duckvil::Memory::linear_capacity(_linear), (size_t)16, "Wrong capacity");
 
         DUCKVIL_TEST_EQUAL(Duckvil::Memory::linear_allocate(__duckvil_global::m_pMemoryInterface, _linear, 1), 1, "Wrong value");
         DUCKVIL_TEST_EQUAL(Duckvil::Memory::linear_allocate(__duckvil_global::m_pMemoryInterface, _linear, 2), 2, "Wrong value");
         DUCKVIL_TEST_EQUAL(Duckvil::Memory::linear_allocate(__duckvil_global::m_pMemoryInterface, _linear, 3), 3, "Wrong value");
         DUCKVIL_TEST_EQUAL(Duckvil::Memory::linear_allocate(__duckvil_global::m_pMemoryInterface, _linear, 4), 4, "Wrong value");
 
-        DUCKVIL_TEST_EXP(_linear->used == 16, "Should be full");
+        DUCKVIL_TEST_EXP(Duckvil::Memory::linear_full(_linear), "Should be full");
+        DUCKVIL_TEST_EQUAL(Duckvil::Memory::linear_size(_linear), (size_t)16, "Wrong size");
     }
 
     DUCKVIL_TEST_SUCCESS_PASS;
