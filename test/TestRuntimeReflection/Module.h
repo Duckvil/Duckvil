9f68c86195918ffaf50ec91e5f632e990dd63acd
mVento3 <aleksander.lipka06@wp.pl>
2020-10-04 23:10:41 +0200

    RuntimeReflection tests

---
+++ test/TestRuntimeReflection/Module.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "PlugNPlay/Module.h"
+
+#include "Memory/Memory.h"
+
+struct __duckvil_global
+{
+    static Duckvil::PlugNPlay::module m_module;
+    static Duckvil::PlugNPlay::__module_information m_memoryModule;
+    static Duckvil::Memory::IMemory* m_pMemoryInterface;
+    static void* m_pMemoryChunk;
+};
---
+++ test/TestRuntimeReflection/TestRuntimeReflection.cpp
@@ -0,0 +1,11 @@
+#include "UniTestFramework/UniTestFramework.h"
+
+DUCKVIL_TEST(RuntimeReflection)
+{
+    DUCKVIL_TEST_SUCCESS_PASS;
+}
+
+int main(int argc, char* argv[])
+{
+    return DUCKVIL_TEST_FAILED;
+}
