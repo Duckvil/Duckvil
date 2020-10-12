2df7594d9430ab563d4c549b8e3ba2c3af20f778
mVento3 <aleksander.lipka06@wp.pl>
2020-10-12 22:18:31 +0200

    Runtime reflection markers

---
+++ include/RuntimeReflection/Markers.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#define DUCKVIL_CLASS(...) // IDK
+#define DUCKVIL_STRUCT(...) // Should be used for classes of structures
+#define DUCKVIL_FUNCTION(...) // Should be used for functions
+#define DUCKVIL_VARIABLE(...) // Should be used for variables
+#define DUCKVIL_NAMESPACE(...) // Should be used for namespaces
+#define DUCKVIL_ARGUMENT(...) // Should be used for arguments
+#define DUCKVIL_CALLBACK(...)
