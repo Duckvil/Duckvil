cb066bd51bd336e0b8668b73b1f97bce1baf55fd
mVento3 <aleksander.lipka06@wp.pl>
2020-10-25 21:42:54 +0100

    Generated reflection files

---
+++ __generated_reflection__/Duckvil/ReflectionFlags.generated.cpp
@@ -0,0 +1,10 @@
+#include "Duckvil\ReflectionFlags.h"
+#include "RuntimeReflection/Recorder.h"
+DUCKVIL_RUNTIME_REFLECTION_RECORD(1)
+{
+using namespace Duckvil::RuntimeReflection;
+using namespace Duckvil;
+DUCKVIL_RESOURCE(type_t) _type;
+DUCKVIL_RESOURCE(property_t) _property;
+DUCKVIL_RESOURCE(constructor_t) _constructor;
+}
---
+++ __generated_reflection__/Logger/Logger.generated.cpp
@@ -0,0 +1,17 @@
+#include "Logger\Logger.h"
+#include "RuntimeReflection/Recorder.h"
+DUCKVIL_RUNTIME_REFLECTION_RECORD(0)
+{
+using namespace Duckvil::RuntimeReflection;
+using namespace Duckvil;
+DUCKVIL_RESOURCE(type_t) _type;
+DUCKVIL_RESOURCE(property_t) _property;
+DUCKVIL_RESOURCE(constructor_t) _constructor;
+using namespace Duckvil::Logger;
+_type = record_type<Duckvil::Logger::__log_info>(DUCKVIL_RUNTIME_REFLECTION_RECORDER_STANDARD_STUFF, "__log_info");
+record_namespace(DUCKVIL_RUNTIME_REFLECTION_RECORDER_STANDARD_STUFF, _type, "Duckvil");
+record_namespace(DUCKVIL_RUNTIME_REFLECTION_RECORDER_STANDARD_STUFF, _type, "Logger");
+_type = record_type<Duckvil::Logger::__ftable>(DUCKVIL_RUNTIME_REFLECTION_RECORDER_STANDARD_STUFF, "__ftable");
+record_namespace(DUCKVIL_RUNTIME_REFLECTION_RECORDER_STANDARD_STUFF, _type, "Duckvil");
+record_namespace(DUCKVIL_RUNTIME_REFLECTION_RECORDER_STANDARD_STUFF, _type, "Logger");
+}
