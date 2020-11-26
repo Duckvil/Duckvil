992878ae537abb3e681507f82792f25affd0f527
mVento3 <aleksander.lipka06@wp.pl>
2020-11-26 17:13:37 +0100

    Generated reflection files

--- __generated_reflection__/Event/Pool.generated.cpp
+++ __generated_reflection__/Event/Pool.generated.cpp
@@ -7,10 +7,4 @@ using namespace Duckvil;
 DUCKVIL_RESOURCE(type_t) _type;
 DUCKVIL_RESOURCE(property_t) _property;
 DUCKVIL_RESOURCE(constructor_t) _constructor;
-using namespace Duckvil::Event;
-_type = record_type<Duckvil::Event::Pool>(DUCKVIL_RUNTIME_REFLECTION_RECORDER_STANDARD_STUFF, "Pool");
-record_namespace(DUCKVIL_RUNTIME_REFLECTION_RECORDER_STANDARD_STUFF, _type, "Duckvil");
-record_namespace(DUCKVIL_RUNTIME_REFLECTION_RECORDER_STANDARD_STUFF, _type, "Event");
-_constructor = record_constructor<Duckvil::Event::Pool>(DUCKVIL_RUNTIME_REFLECTION_RECORDER_STANDARD_STUFF, _type);
-_constructor = record_constructor<Duckvil::Event::Pool, const Memory::FreeList&, RuntimeReflection::__data*>(DUCKVIL_RUNTIME_REFLECTION_RECORDER_STANDARD_STUFF, _type);
 }
