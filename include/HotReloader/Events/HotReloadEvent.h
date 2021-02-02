80b02700c3a309fbf4c703dfef3aeae6e9915695
mVento3 <aleksander.lipka06@wp.pl>
2021-02-02 17:13:37 +0100

    HotReload event change

--- include/HotReloader/Events/HotReloadEvent.h
+++ include/HotReloader/Events/HotReloadEvent.h
@@ -2,6 +2,8 @@
 
 #include <cstdint>
 
+#include "RuntimeReflection/RuntimeReflection.h"
+
 namespace Duckvil { namespace HotReloader {
 
     struct HotReloadedEvent
@@ -10,16 +12,20 @@ namespace Duckvil { namespace HotReloader {
         {
             stage_not_specified,
             stage_before_compile,
-            stage_after_compile
+            stage_after_compile,
+            stage_after_swap
             // And more...
         };
 
         HotReloadedEvent()
         {
             m_stage = stage::stage_not_specified;
+            m_pObject = nullptr;
         }
 
+        void* m_pObject;
         HotReloadedEvent::stage m_stage;
+        RuntimeReflection::__duckvil_resource_type_t _typeHandle;
     };
 
 }}
