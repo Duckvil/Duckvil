84e7a1f7baa7d4674ed2b279fcd5ac8bc82f7f5f
mVento3 <aleksander.lipka06@wp.pl>
2021-03-06 18:11:36 +0100

    Few new meta functions

--- include/RuntimeReflection/Meta.h
+++ include/RuntimeReflection/Meta.h
@@ -22,6 +22,12 @@
     } \
  \
     template <typename KeyType> \
+    static inline void* get_meta_value_ptr(__ftable* _pReflection, __data* _pData, type, const KeyType& _key) \
+    { \
+        return _pReflection->m_fnGet ## name ## MetaValue(_pData, type2, &_key, sizeof(KeyType), typeid(KeyType).hash_code()); \
+    } \
+ \
+    template <typename KeyType> \
     static inline const __variant& get_meta(__ftable* _pReflection, __data* _pData, type, const KeyType& _key) \
     { \
         return _pReflection->m_fnGet ## name ## MetaVariant(_pData, type2, &_key, sizeof(KeyType), typeid(KeyType).hash_code()); \
@@ -31,6 +37,12 @@
     static inline const ValueType& get_meta_value(__ftable* _pReflection, __data* _pData, type, const char (&_key)[Length]) \
     { \
         return *(ValueType*)_pReflection->m_fnGet ## name ## MetaValue(_pData, type2, _key, Length, CONST_CHAR_POINTER_ID); \
+    } \
+ \
+    template <std::size_t Length> \
+    static inline void* get_meta_value_ptr(__ftable* _pReflection, __data* _pData, type, const char (&_key)[Length]) \
+    { \
+        return _pReflection->m_fnGet ## name ## MetaValue(_pData, type2, _key, Length, CONST_CHAR_POINTER_ID); \
     } \
  \
     template <typename KeyType> \
@@ -48,6 +60,13 @@
     } \
  \
     template <typename KeyType> \
+    static inline void* get_meta_value_ptr(type, const KeyType& _key) \
+    { \
+        const duckvil_frontend_reflection_context& _context = g_duckvilFrontendReflectionContext; \
+        return get_meta_value_ptr<KeyType>(_context.m_pReflection, _context.m_pReflectionData, type2, _key); \
+    } \
+ \
+    template <typename KeyType> \
     static inline const __variant& get_meta(type, const KeyType& _key) \
     { \
         const duckvil_frontend_reflection_context& _context = g_duckvilFrontendReflectionContext; \
@@ -59,9 +78,17 @@
     { \
         const duckvil_frontend_reflection_context& _context = g_duckvilFrontendReflectionContext; \
         return get_meta_value<ValueType>(_context.m_pReflection, _context.m_pReflectionData, type2, _key); \
+    } \
+ \
+    template <std::size_t Length> \
+    static inline void* get_meta_value_ptr(type, const char (&_key)[Length]) \
+    { \
+        const duckvil_frontend_reflection_context& _context = g_duckvilFrontendReflectionContext; \
+        return get_meta_value_ptr(_context.m_pReflection, _context.m_pReflectionData, type2, _key); \
     }
 
 namespace Duckvil { namespace RuntimeReflection {
+
 
     DUCKVIL_META_UTIL(Type, DUCKVIL_RESOURCE(type_t) _typeHandle, _typeHandle);
     DUCKVIL_META_UTIL(Property, DUCKVIL_META_CAT(DUCKVIL_RESOURCE(type_t) _typeHandle, DUCKVIL_RESOURCE(property_t) _propertyHandle), DUCKVIL_META_CAT(_typeHandle, _propertyHandle));
