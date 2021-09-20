b4814dee7848e49902f8dc7e50a595c3306e3c8d
mVento3 <aleksander.lipka06@wp.pl>
2021-09-20 21:59:39 +0200

    Added xbyak submodule, dynamic arguments push feature

---
+++ include/Utils/FunctionArgumentsPusher.h
@@ -0,0 +1,105 @@
+#pragma once
+
+#include "xbyak/xbyak.h"
+
+namespace Duckvil {
+
+    struct FunctionArgumentsPusher : Xbyak::CodeGenerator
+    {
+        uint32_t m_uiIndex = 0;
+        bool m_aFloats[4];
+        uint32_t m_uiCount;
+
+        explicit FunctionArgumentsPusher(uint32_t _uiCount);
+        ~FunctionArgumentsPusher();
+
+        void Push(const int& _data);
+        void Push(const size_t& _data);
+        void Push(const float& _data);
+        void Push(const double& _data);
+        void Push(const bool& _data);
+
+        template <typename Type>
+        void Push(const Type& _data)
+        {
+            if(m_uiIndex < 4)
+            {
+                m_aFloats[m_uiIndex] = false;
+            }
+
+            m_uiIndex++;
+
+            if(m_uiIndex == 1)
+            {
+                push(rcx);
+                mov(rcx, (size_t)&_data);
+            }
+            else if(m_uiIndex == 2)
+            {
+                push(rdx);
+                mov(rdx, (size_t)&_data);
+            }
+            else if(m_uiIndex == 3)
+            {
+                push(r8);
+                mov(r8, (size_t)&_data);
+            }
+            else if(m_uiIndex == 4)
+            {
+                push(r9);
+                mov(r9, (size_t)&_data);
+            }
+            else
+            {
+                mov(r10, size_t(&_data));
+                mov(qword[rsp + 24 + (8 * (m_uiIndex - 5))], r10);
+            }
+        }
+
+        template <typename Type>
+        void Push(Type* _data)
+        {
+            if(m_uiIndex < 4)
+            {
+                m_aFloats[m_uiIndex] = false;
+            }
+
+            m_uiIndex++;
+
+            if(m_uiIndex == 1)
+            {
+                push(rcx);
+                mov(rcx, (size_t)_data);
+            }
+            else if(m_uiIndex == 2)
+            {
+                push(rdx);
+                mov(rdx, (size_t)_data);
+            }
+            else if(m_uiIndex == 3)
+            {
+                push(r8);
+                mov(r8, (size_t)_data);
+            }
+            else if(m_uiIndex == 4)
+            {
+                push(r9);
+                mov(r9, (size_t)_data);
+            }
+            else
+            {
+                mov(r10, size_t(_data));
+                mov(qword[rsp + 24 + (8 * (m_uiIndex - 5))], r10);
+            }
+        }
+
+        void Call(void* func);
+
+        template <typename R, typename C, typename... Args>
+        void Call(R (C::*_pFunc)(Args...))
+        {
+            Call((void*&)_pFunc);
+        }
+    };
+
+}
---
+++ source/Utils/FunctionArgumentsPusher.cpp
@@ -0,0 +1,261 @@
+#include "Utils/FunctionArgumentsPusher.h"
+
+namespace Duckvil {
+
+    FunctionArgumentsPusher::FunctionArgumentsPusher(uint32_t _uiCount) :
+        m_uiCount(_uiCount)
+    {
+        // push(rbp);
+        // mov(rbp, rsp);
+
+        if(_uiCount > 4)
+        {
+            sub(rsp, 24 + (8 * (_uiCount - 4)));
+        }
+    }
+
+    FunctionArgumentsPusher::~FunctionArgumentsPusher()
+    {
+
+    }
+
+    void FunctionArgumentsPusher::Push(const int& _data)
+    {
+        if(m_uiIndex < 4)
+        {
+            m_aFloats[m_uiIndex] = false;
+        }
+
+        m_uiIndex++;
+
+        if(m_uiIndex == 1)
+        {
+            push(rcx);
+            mov(rcx, _data);
+        }
+        else if(m_uiIndex == 2)
+        {
+            push(rdx);
+            mov(rdx, _data);
+        }
+        else if(m_uiIndex == 3)
+        {
+            push(r8);
+            mov(r8, _data);
+        }
+        else if(m_uiIndex == 4)
+        {
+            push(r9);
+            mov(r9, _data);
+        }
+        else
+        {
+            mov(qword[rsp + 24 + (8 * (m_uiIndex - 5))], _data);
+        }
+    }
+
+    void FunctionArgumentsPusher::Push(const size_t& _data)
+    {
+        if(m_uiIndex < 4)
+        {
+            m_aFloats[m_uiIndex] = false;
+        }
+
+        m_uiIndex++;
+
+        if(m_uiIndex == 1)
+        {
+            push(rcx);
+            mov(rcx, _data);
+        }
+        else if(m_uiIndex == 2)
+        {
+            push(rdx);
+            mov(rdx, _data);
+        }
+        else if(m_uiIndex == 3)
+        {
+            push(r8);
+            mov(r8, _data);
+        }
+        else if(m_uiIndex == 4)
+        {
+            push(r9);
+            mov(r9, _data);
+        }
+        else
+        {
+            mov(qword[rsp + 24 + (8 * (m_uiIndex - 5))], _data);
+        }
+    }
+
+    void FunctionArgumentsPusher::Push(const float& _data)
+    {
+        if(m_uiIndex < 4)
+        {
+            m_aFloats[m_uiIndex] = true;
+        }
+
+        m_uiIndex++;
+
+        if(m_uiIndex == 1)
+        {
+            mov(r10, size_t(&_data));
+            movdqu(xmm0, ptr[r10]);
+        }
+        else if(m_uiIndex == 2)
+        {
+            mov(r10, size_t(&_data));
+            movdqu(xmm1, ptr[r10]);
+        }
+        else if(m_uiIndex == 3)
+        {
+            mov(r10, size_t(&_data));
+            movdqu(xmm2, ptr[r10]);
+        }
+        else if(m_uiIndex == 4)
+        {
+            mov(r10, size_t(&_data));
+            movdqu(xmm3, ptr[r10]);
+        }
+        else
+        {
+            mov(r10, size_t(&_data));
+            mov(r10, ptr[r10]);
+            mov(qword[rsp + 24 + (8 * (m_uiIndex - 5))], r10);
+        }
+    }
+
+    void FunctionArgumentsPusher::Push(const double& _data)
+    {
+        if(m_uiIndex < 4)
+        {
+            m_aFloats[m_uiIndex] = true;
+        }
+
+        m_uiIndex++;
+
+        if(m_uiIndex == 1)
+        {
+            mov(r10, size_t(&_data));
+            movdqu(xmm0, ptr[r10]);
+        }
+        else if(m_uiIndex == 2)
+        {
+            mov(r10, size_t(&_data));
+            movdqu(xmm1, ptr[r10]);
+        }
+        else if(m_uiIndex == 3)
+        {
+            mov(r10, size_t(&_data));
+            movdqu(xmm2, ptr[r10]);
+        }
+        else if(m_uiIndex == 4)
+        {
+            mov(r10, size_t(&_data));
+            movdqu(xmm3, ptr[r10]);
+        }
+        else
+        {
+            mov(r10, size_t(&_data));
+            mov(r10, ptr[r10]);
+            mov(qword[rsp + 24 + (8 * (m_uiIndex - 5))], r10);
+        }
+    }
+
+    void FunctionArgumentsPusher::Push(const bool& _data)
+    {
+        if(m_uiIndex < 4)
+        {
+            m_aFloats[m_uiIndex] = false;
+        }
+
+        m_uiIndex++;
+
+        if(m_uiIndex == 1)
+        {
+            push(rcx);
+            mov(rcx, _data);
+        }
+        else if(m_uiIndex == 2)
+        {
+            push(rdx);
+            mov(rdx, _data);
+        }
+        else if(m_uiIndex == 3)
+        {
+            push(r8);
+            mov(r8, _data);
+        }
+        else if(m_uiIndex == 4)
+        {
+            push(r9);
+            mov(r9, _data);
+        }
+        else
+        {
+            mov(qword[rsp + 24 + (8 * (m_uiIndex - 5))], _data);
+        }
+    }
+
+    void FunctionArgumentsPusher::Call(void* func)
+    {
+        push(rbx);
+        mov(rbx, size_t(func));
+        call(rbx);
+        pop(rbx);
+
+        uint32_t _index = m_uiIndex > 4 ? 4 : m_uiIndex;
+
+        if(m_uiIndex >= 4)
+        {
+            _index--;
+
+            if(m_aFloats[_index] == false)
+            {
+                pop(r9);
+            }
+        }
+
+        if(m_uiIndex >= 3)
+        {
+            _index--;
+
+            if(m_aFloats[_index] == false)
+            {
+                pop(r8);
+            }
+        }
+
+        if(m_uiIndex >= 2)
+        {
+            _index--;
+
+            if(m_aFloats[_index] == false)
+            {
+                pop(rdx);
+            }
+        }
+
+        if(m_uiIndex >= 1)
+        {
+            _index--;
+
+            if(m_aFloats[_index] == false)
+            {
+                pop(rcx);
+            }
+        }
+
+        if(m_uiCount > 4)
+        {
+            add(rsp, 24 + (8 * (m_uiCount - 4)));
+        }
+
+        // mov(rsp, rbp);
+        // pop(rbp);
+
+        ret();
+    }
+
+}
