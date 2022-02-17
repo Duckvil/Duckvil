d06fbb1aa6d0994bfeb920fca576bb2f6daf1a2a
mVento3 <aleksander.lipka06@wp.pl>
2022-02-17 20:20:11 +0100

    Added delta time unit

--- source/Editor/Widgets/EngineInfoWidget.cpp
+++ source/Editor/Widgets/EngineInfoWidget.cpp
@@ -31,7 +31,7 @@ namespace Duckvil { namespace Editor {
     {
         ImGui::Begin("EngineInfoWidget");
 
-        ImGui::Text("Delta: %f", m_pTime->m_dDelta * 1000.f);
+        ImGui::Text("Delta: %f ms", m_pTime->m_dDelta * 1000.f);
         ImGui::Text("Memory used: %i of: %i", m_heap.GetAllocator()->m_ullUsed, m_heap.GetAllocator()->m_ullCapacity);
         // ImGui::Text("AAAAA");
 
